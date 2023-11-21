5fd7af800c7d7e0f00a4ac82cc6f644120441127
ajbmendoza <ajbmendoza@addu.edu.ph>
2023-11-21 19:16:56 +0800

    first commit

---
+++ src/app/posts/page.js
@@ -0,0 +1,12 @@
+import Post from '@/components/Post'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Post title = 'Lorem Ipsum' author='Beethoven' body='Hey this is my first post'></Post>
+    </div>
+  )
+}
+
+export default page
